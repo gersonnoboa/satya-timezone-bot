fa530575889d8d3e9aa0854f61314eecad1c5401
Gerson Noboa <gerson@noboa.dev>
2024-11-02 17:13:31 +0200

    Fix regex

--- src/time-conversion/TimeChecker.cs
+++ src/time-conversion/TimeChecker.cs
@@ -6,7 +6,7 @@ namespace Dev.Noboa
 	{
 		public static bool IsTimeCorrect(string time)
 		{
-			var regexPattern = "\b((1[0-2]|0?[1-9]):([0-5][0-9]) ?([AaPp][Mm])?|([01]?[0-9]|2[0-3]):[0-5][0-9]|(1[0-2]|0?[1-9]) ?([AaPp][Mm]))\b";
+			var regexPattern = @"\b((1[0-2]|0?[1-9]):([0-5][0-9]) ?([AaPp][Mm])?|([01]?[0-9]|2[0-3]):[0-5][0-9]|(1[0-2]|0?[1-9]) ?([AaPp][Mm]))\b";
 			return Regex.IsMatch(time, regexPattern);
 		}
 	}
