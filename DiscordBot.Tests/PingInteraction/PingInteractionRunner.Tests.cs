9e088612ad7869fe616cc28a959ddc08cbc79100
Gerson Noboa <gerson@noboa.dev>
2025-01-18 10:53:14 +0200

    Add preliminary work for unit testing

---
+++ DiscordBot.Tests/PingInteraction/PingInteractionRunner.Tests.cs
@@ -0,0 +1,21 @@
+using DiscordBot.PingInteraction;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace DiscordBot.Tests.PingInteraction;
+
+[TestClass]
+public class PingInteractionRunnerTests
+{
+    private readonly ILogger<PingInteractionRunner> _logger = NullLogger<PingInteractionRunner>.Instance;
+    
+    [TestMethod]
+    public void Test()
+    {
+        var expected = new JsonResult(new { type = 1 });
+        var actual = PingInteractionRunner.Run(_logger);
+        
+        Assert.AreEqual(expected.ToString(), actual.ToString());
+    }
+}
